60af17110854c39ce385d994b624f98ff5e39d7c
stefan judis <stefanjudis@gmail.com>
2022-10-13 15:34:18 +0200

    yolo

--- __checks__/dogs-not-found.spec.js
+++ __checks__/dogs-not-found.spec.js
@@ -1,19 +1,21 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test")
 
-test('404 page is doggified', async ({ page }) => {
-  await page.goto('https://www.stefanjudis.com/NOT_FOUND/');
+test("404 page is doggified", async ({ page }) => {
+  await page.goto("https://www.stefanjudis.com/NOT_FOUND/")
 
-  await expect(page).toHaveTitle(/Not found/);
+  await expect(page).toHaveTitle(/Not found/)
 
-  const videoSource = page.locator('video > source');
-  await expect(videoSource).toHaveAttribute('src', /^\/\/videos.ctfassets.net/);
-  const firstVideoURL = await videoSource.getAttribute('src');
+  const videoSource = page.locator("video > source")
+  await expect(videoSource).toHaveAttribute("src", /^\/\/videos.ctfassets.net/)
+  const firstVideoURL = await videoSource.getAttribute("src")
 
-  const changeVideoBtn = page.locator('text="Okay, show me another one!"');
-  await changeVideoBtn.click();
+  const changeVideoBtn = page.locator('text="Okay, show me another one!"')
+  await changeVideoBtn.click()
 
-  const secondVideoURL = await videoSource.getAttribute('src');
+  const secondVideoURL = await videoSource.getAttribute("src")
 
-  expect(firstVideoURL).not.toBe(secondVideoURL);
-});
+  // hello lisa!!!
+
+  expect(firstVideoURL).not.toBe(secondVideoURL)
+})
